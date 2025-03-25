ffc9d78b5c007f320fd75122ed0c6284995beea2
Qondzio <qondziolabaziewicz@gmail.com>
2025-03-25 14:28:04 +0100

    Corrected function to display numbers and add a function to clear the numbers.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const buttons=document.querySelectorAll("button");
-const sum=document.querySelector(".sum p");
+let sum=document.querySelector(".sum p");
 let lastOperator;
 let lastNumer;
 
 buttons.forEach((button)=> {button.addEventListener('click', ()=>{
     const clickedButton=event.target.value;
 
-    displayValue(clickedButton);
+    switch (clickedButton){
+        case "c":
+            sum.textContent="0";
+            break;
+        default:
+            displayValue(clickedButton);
+    }
+    
     
     })
 })
@@ -15,14 +22,23 @@ buttons.forEach((button)=> {button.addEventListener('click', ()=>{
 //Function to display clicked number on the screen.
 function displayValue(clickedButton)
 {
-    clickedButton=parseInt(clickedButton);
-    if (Number.isInteger(clickedButton)) //Prevent to display parameters on screen.
-    if(sum.textContent==="0")
-        {
-            sum.textContent=clickedButton;
-        }
-        else
-        {
-            sum.textContent+=clickedButton;
-        }
+    if(clickedButton==="," && sum.textContent.includes(",")===false)  //Prevent displaying "," character more than once.
+    {
+        sum.textContent+=clickedButton;
+    }
+    else
+    {
+        clickedButton=parseInt(clickedButton);
+        
+        if (Number.isInteger(clickedButton)) //Prevent to display parameters on screen.
+        if(sum.textContent==="0")
+            {
+                sum.textContent=clickedButton;
+            }
+            else
+            {
+                sum.textContent+=clickedButton;
+            }
+    }
 }
+
