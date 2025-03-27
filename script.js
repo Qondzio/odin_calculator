0081045e827c62687c8f79fb1433038f06aa4228
Qondzio <qondziolabaziewicz@gmail.com>
2025-03-27 14:49:05 +0100

    Finished every single thing in calculator.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const buttons=document.querySelectorAll("button");
 let sum=document.querySelector(".sum p");
 let lastOperator;
-let lastNumer;
+let lastButton;
+let lastNumber=0;
+let currentNumber;
 
 buttons.forEach((button)=> {button.addEventListener('click', ()=>{
     const clickedButton=event.target.value;
@@ -9,36 +11,102 @@ buttons.forEach((button)=> {button.addEventListener('click', ()=>{
     switch (clickedButton){
         case "c":
             sum.textContent="0";
+            lastOperator=undefined;
+            lastButton=undefined;
+            lastNumber=0;
+            currentNumber=undefined;
+            break;
+        case "+/-":
+            if(sum.textContent.includes("-")===false){
+                sum.textContent="-"+sum.textContent;
+            }
+            else{
+                sum.textContent=sum.textContent.replace("-","");
+            }
+            break;
+        case "%":
+            sum.textContent=parseFloat(sum.textContent)/100;
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            lastOperator=clickedButton;
+            break;
+        case "/":
+            returnResult();
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            lastOperator=clickedButton;
+            break;
+        case "*":
+            returnResult();
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            lastOperator=clickedButton;
+            break;
+        case "-":
+            returnResult();
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            lastOperator=clickedButton;
+            break;
+        case "+":
+            returnResult();
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            lastOperator=clickedButton;
+            break;
+        case "=":
+            returnResult();
+            lastNumber=parseFloat(sum.textContent);
+            lastButton=clickedButton;
+            break;
+        case ".":
+            if(sum.textContent.includes(".")===false)
+            {
+                sum.textContent+=".";
+            }
+            lastButton=".";
             break;
         default:
-            displayValue(clickedButton);
+            updateDisplay(clickedButton);
+            lastButton=clickedButton;
     }
     
+    if(sum.textContent.length>9){
+        sum.textContent=sum.textContent.substring(0,9);
+    }
     
     })
 })
 
 
+
 //Function to display clicked number on the screen.
-function displayValue(clickedButton)
+function updateDisplay(clickedButton)
 {
-    if(clickedButton==="," && sum.textContent.includes(",")===false)  //Prevent displaying "," character more than once.
-    {
-        sum.textContent+=clickedButton;
+    if(sum.textContent==="0" || lastButton==="/" || lastButton==="*" || lastButton==="-" || lastButton==="+"){
+        sum.textContent=clickedButton;
     }
-    else
-    {
-        clickedButton=parseInt(clickedButton);
-        
-        if (Number.isInteger(clickedButton)) //Prevent to display parameters on screen.
-        if(sum.textContent==="0")
-            {
-                sum.textContent=clickedButton;
-            }
-            else
-            {
-                sum.textContent+=clickedButton;
-            }
+    else{
+        sum.textContent+=clickedButton;
     }
 }
 
+
+function returnResult(){
+    if(Number.isInteger(parseInt(lastButton))===true){
+        currentNumber=parseFloat(sum.textContent);
+        switch (lastOperator){
+            case "+":
+                return sum.textContent=(lastNumber+currentNumber).toString();
+            case "-":
+                return sum.textContent=(lastNumber-currentNumber).toString();
+            case "*":
+                return sum.textContent=(lastNumber*currentNumber).toString();
+            case "/":
+                return sum.textContent=(lastNumber/currentNumber).toString();
+            default:
+                
+        }
+    }
+        
+}
